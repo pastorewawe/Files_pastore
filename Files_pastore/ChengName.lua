8b414ab8543e8e738ef87f374a39e433f7835986
pastorewawe <84621878+pastorewawe@users.noreply.github.com>
2021-05-26 02:47:49 +0300

    Add files via upload

--- Files_pastore/ChengName.lua
+++ Files_pastore/ChengName.lua
@@ -5,11 +5,11 @@ if text then
 tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data)
 if data.id_ then 
 if data.id_ ~= bot_id then
-local pastoreChengName = database:get(bot_id.."pastore:Cheng:Name"..data.id_)
+local pastoreChengName = redis:get(bot_id.."pastore:Cheng:Name"..data.id_)
 if not data.first_name_ then 
 if pastoreChengName then 
 send(msg.chat_id_, msg.id_, "يولد هذا غير اسمه لك ها كلب  ["..pastoreChengName..']')
-database:del(bot_id.."pastore:Cheng:Name"..data.id_) 
+redis:del(bot_id.."pastore:Cheng:Name"..data.id_) 
 end
 end
 if data.first_name_ then 
@@ -23,7 +23,7 @@ local Text = {
 }
 send(msg.chat_id_, msg.id_,Text[math.random(#Text)])
 end  
-database:set(bot_id.."pastore:Cheng:Name"..data.id_, data.first_name_) 
+redis:set(bot_id.."pastore:Cheng:Name"..data.id_, data.first_name_) 
 end
 end
 end
--- Files_pastore/ChengUser.lua
+++ Files_pastore/ChengUser.lua
@@ -5,11 +5,11 @@ if text then
 tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data)
 if data.id_ then 
 if data.id_ ~= bot_id then
-local rakon0ChengUserName = database:get(bot_id.."rakon0:Cheng:UserName"..data.id_)
+local rakon0ChengUserName = redis:get(bot_id.."rakon0:Cheng:UserName"..data.id_)
 if not data.username_ then 
 if rakon0ChengUserName then 
 send(msg.chat_id_, msg.id_, 1, "حذف معرفه خمطو بساع بساع  \n هاذه معرفه  : [@"..rakon0ChengUserName..']')
-database:del(bot_id.."pastore:Cheng:UserName"..data.id_) 
+redis:del(bot_id.."pastore:Cheng:UserName"..data.id_) 
 end
 end
 if data.username_ then 
@@ -25,7 +25,7 @@ local Text = {
 }
 send(msg.chat_id_, msg.id_,Text[math.random(#Text)])
 end  
-database:set(bot_id.."rakon0:Cheng:UserName"..data.id_, data.username_) 
+redis:set(bot_id.."rakon0:Cheng:UserName"..data.id_, data.username_) 
 end
 end
 end
